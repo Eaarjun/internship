49b34b7b7966171cf03a92b0a8567fdda878f215
Eaarjun <91949706+Eaarjun@users.noreply.github.com>
2023-09-19 22:42:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(
     cors({
-   origin: "https://internship-alpha-seven.vercel.app/"
+   origin: "https://travel-blog-5x6n.onrender.com/"
 })
 )
 
