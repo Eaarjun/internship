94ea0d3513a04599b3a15a3833cfa0318beee6fe
Eaarjun <91949706+Eaarjun@users.noreply.github.com>
2023-09-20 07:12:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(
     })
 )
 
-mongoose.connect(process.env['DB_URL'], { useNewUrlParser: true })
+mongoose.connect(mongodb+srv://admin:CTsoVT1hpVIjNNZX@cluster0.pocdklc.mongodb.net/, { useNewUrlParser: true })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
