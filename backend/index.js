f7b8e7b155c0f3e30e6a086ad23e89a7039878d5
Eaarjun <91949706+Eaarjun@users.noreply.github.com>
2023-09-20 06:57:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(
     cors({
-    origin:"https://internship-react-eosin.vercel.app"
+    origin:"http://localhost:5173"
     })
 )
 
